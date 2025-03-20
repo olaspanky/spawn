02e22320d03e659ef8ac68a0b6ce27957f4229f7
Olakareem <olakareemomobolarinwa@gmail.com>
2025-03-20 23:17:35 +0100

    fix

--- src/app/lib/api.ts
+++ src/app/lib/api.ts
@@ -16,13 +16,13 @@ export const getOrderById = (orderId: string, token: string) =>
   api.get<Order>(`/purchases/${orderId}`, { headers: { 'x-auth-token': token } }).then(res => res.data);
 
 export const scheduleMeeting = (orderId: string, location: string, time: string, token: string) =>
-  api.post(`/purchases/${orderId}/schedule`, { location, time }, { headers: { 'x-auth-token': token } }).then(res => res.data);
+  api.post(`/purchases/${orderId}/schedule-meeting`, { location, time }, { headers: { 'x-auth-token': token } }).then(res => res.data);
 
 export const releaseFunds = (orderId: string, token: string) =>
-  api.post(`/purchases/${orderId}/release`, {}, { headers: { 'x-auth-token': token } }).then(res => res.data);
+  api.post(`/purchases/${orderId}/release-funds`, {}, { headers: { 'x-auth-token': token } }).then(res => res.data);
 
 export const retractFunds = (orderId: string, reason: string, token: string) =>
-  api.post(`/purchases/${orderId}/retract`, { reason }, { headers: { 'x-auth-token': token } }).then(res => res.data);
+  api.post(`/purchases/${orderId}/retract-funds`, { reason }, { headers: { 'x-auth-token': token } }).then(res => res.data);
 
 export const rateSeller = (orderId: string, rating: number, token: string) =>
-  api.post(`/purchases/${orderId}/rate`, { rating }, { headers: { 'x-auth-token': token } }).then(res => res.data);
+  api.post(`/purchases/${orderId}/rate-seller`, { rating }, { headers: { 'x-auth-token': token } }).then(res => res.data);
