ced867ee430f98a974d7a0b7ea404fb023b5209a
olakareem <olakareemomobolarinwa@gmail.com>
2026-04-06 16:57:57 +0100

    fix

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,67 +1,15 @@
 import type { Config } from "tailwindcss";
-import daisyui from 'daisyui';
 
-export default {
+const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-        primary: {
-          light: '#1A1A1A', // blue-500
-          dark: '#1A1A1A',  // blue-400
-        },
-        gray: {
-          600: '#1A1A1A', // Primary brand color
-        }
-      },
-      fontFamily: {
-        font1: ["var(--font-montserrat)", "sans-serif"],
-        font2: ["var(--font-robo-slab)", "serif"],
-      },
-
-       
-    },
-  },
-  darkMode: 'class', // This enables the dark mode variant
-  plugins: [daisyui],
   daisyui: {
-    themes: [
-      "light",
-      "dark",
-      "cupcake",
-      "bumblebee",
-      "corporate",
-      "synthwave",
-      "retro",
-      "cyberpunk",
-      "valentine",
-      "halloween",
-      "garden",
-      "forest",
-      "aqua",
-      "lofi",
-      "pastel",
-      "fantasy",
-      "wireframe",
-      "black",
-      "luxury",
-      "dracula",
-      "cmyk",
-      "autumn",
-      "business",
-      "acid",
-      "lemonade",
-      "night",
-      "coffee",
-      "winter",
-      "dim",
-      "nord",
-      "sunset",
-    ],
-  },} satisfies Config;
+    themes: ["light", "dark"], // change or add your themes here
+  },
+  plugins: [], // ← Remove any import of daisyui here
+};
+
+export default config;
