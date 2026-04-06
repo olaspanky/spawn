ab26a77cedd94e68425a96ebba6c271215992b12
olakareem <olakareemomobolarinwa@gmail.com>
2026-04-06 16:43:21 +0100

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,28 +1,3 @@
-// next.config.ts
 import type { NextConfig } from "next";
-const withPWA = require("next-pwa")({
-  dest: "public",
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === "development", // Disable in dev to avoid caching hell
-  fallbacks: {
-    document: "/offline", // Your offline fallback route
-  },
-});
-
-const nextConfig: NextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "res.cloudinary.com",
-        pathname: "/**",
-      },
-    ],
-  },
-};
-
-export default withPWA(nextConfig);
+const nextConfig: NextConfig = {};
+export default nextConfig;
